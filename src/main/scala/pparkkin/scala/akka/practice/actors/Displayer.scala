ecc4fce472bce6c0bbf33888fa70a26d963f7ae1
Paavo <pparkkin@gmail.com>
2013-01-03 09:11:20 +0900

    Add Displayer actor.

---
+++ src/main/scala/pparkkin/scala/akka/practice/actors/Displayer.scala
@@ -0,0 +1,15 @@
+package pparkkin.scala.akka.practice.actors
+
+import akka.actor.{ActorLogging, Actor}
+
+case class Display(information: Seq[Int])
+
+class Displayer extends Actor with ActorLogging {
+
+  def receive = {
+    case Display(i) =>
+      log.info("Got: "+i)
+    case msg =>
+      log.error("Received unknown message: "+msg)
+  }
+}
