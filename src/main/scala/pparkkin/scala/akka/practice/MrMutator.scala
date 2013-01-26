97ef53a18647bd66d3c0df3442d09e4cb9205ccb
Paavo <pparkkin@gmail.com>
2013-01-26 09:29:11 +0900

    Draw some triangles.

--- src/main/scala/pparkkin/scala/akka/practice/MrMutator.scala
+++ src/main/scala/pparkkin/scala/akka/practice/MrMutator.scala
@@ -5,9 +5,10 @@ import java.io.File
 import model.GeneticInformation
 import swing._
 import java.awt.image.BufferedImage
-import java.awt.{Color, Graphics2D, Dimension}
+import java.awt.{Polygon, Color, Graphics2D, Dimension}
 import scala.Some
 import javax.swing.Box
+import collection.immutable
 
 object MrMutator extends SimpleSwingApplication {
 
@@ -18,10 +19,11 @@ object MrMutator extends SimpleSwingApplication {
         super.main(args)
         targetPanel.setImage(img)
         val range = math.max(img.getHeight, img.getWidth)
-        val gi = GeneticInformation.random(range - (range % 7))
-        val curF = currentFrame(currentPanel(img.getWidth, img.getHeight, gi))
+        val gi = GeneticInformation.random(range - (range % GeneticInformation.SET_SIZE))
+        val panel = currentPanel(img.getWidth, img.getHeight, gi)
+        val curF = currentFrame(panel)
         curF.visible = true
-//        val mutatorSystem = new MutatorSystem("MrMutator", img, gi)
+//        val mutatorSystem = new MutatorSystem("MrMutator", img, gi, panel)
 //        mutatorSystem.run()
       }
       case None => {}
@@ -76,6 +78,49 @@ object MrMutator extends SimpleSwingApplication {
     def paintComponent(g: Graphics2D) {
       g.setColor(Color.BLACK)
       g.fillRect(0, 0, size.width, size.height)
+      fillPolygons(g, gi.data)
+    }
+
+    def fillPolygons(g: Graphics2D, data: immutable.IndexedSeq[Int]) {
+      if (data.length < GeneticInformation.SET_SIZE) return
+
+      val c = constructColor(data)
+      val p = constructPolygon(data.drop(4))
+
+      (c, p) match {
+        case (Some(color), Some(polygon)) => {
+          g.setColor(color)
+          g.fillPolygon(polygon)
+          //println("Polygon")
+        }
+      }
+
+      fillPolygons(g, data.drop(GeneticInformation.SET_SIZE))
+    }
+
+    def constructColor(data: immutable.IndexedSeq[Int]): Option[Color] = {
+      if (data.length < 4) None
+
+      val r = data(0)
+      val g = data(1)
+      val b = data(2)
+      val a = data(3)
+
+      print(r, g, b, a)
+
+      Some(new Color(r, g, b, a))
+    }
+
+    def constructPolygon(data: immutable.IndexedSeq[Int]): Option[Polygon] = {
+      if (data.length < 6) None
+
+      val xs = data.take(3)
+      val ys = data.drop(3).take(3)
+
+      print(" -> ")
+      println(xs, ys)
+
+      Some(new Polygon(xs.toArray, ys.toArray, xs.length))
     }
   }
 
--- src/main/scala/pparkkin/scala/akka/practice/model/GeneticInformation.scala
+++ src/main/scala/pparkkin/scala/akka/practice/model/GeneticInformation.scala
@@ -3,11 +3,19 @@ package pparkkin.scala.akka.practice.model
 import collection.immutable
 import util.Random
 
-class GeneticInformation(data: immutable.IndexedSeq[Int])
+class GeneticInformation(val data: immutable.IndexedSeq[Int])
+
+// The data in the genetic information is simply a list integers with
+// length % 10 == 0. So n sets of 10 integers.
+// Each set starts with four values representing a color: r, g, b, a
+// Next three values are the x points, and the last three are the y points.
+// Each set is so a coloured triangle in a 2D space.
 
 object GeneticInformation {
+  val SET_SIZE = 10
+
   def random(len: Int): GeneticInformation = {
-    assert(len % 7 == 0)
+    assert(len % SET_SIZE == 0)
 
     new GeneticInformation(1 to len map (_ => Random.nextInt(256)))
   }
