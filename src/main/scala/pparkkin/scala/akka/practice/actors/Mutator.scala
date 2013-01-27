e92fa39c1b727dae25bafae0ddc33668e871f336
Paavo <pparkkin@gmail.com>
2013-01-27 18:19:56 +0900

    Improve mutation.

--- src/main/scala/pparkkin/scala/akka/practice/actors/Mutator.scala
+++ src/main/scala/pparkkin/scala/akka/practice/actors/Mutator.scala
@@ -4,7 +4,7 @@ import akka.actor.{ActorRef, ActorLogging, Actor}
 import collection.immutable
 import util.Random
 import collection.immutable.Stream.Empty
-import pparkkin.scala.akka.practice.model.GeneticMaterial
+import pparkkin.scala.akka.practice.model.{GeneticSequence, GeneticMaterial}
 
 case object Mutate
 //case class Mutated(information: immutable.IndexedSeq[Float])
@@ -15,23 +15,30 @@ class Mutator(gm: GeneticMaterial, selector: ActorRef) extends Actor with ActorL
     case Mutate => {
       log.debug("Get top genetic material, mutate, and send to Selector")
       val m = gm.head
-      selector ! Select(GeneticMaterial.randomSequence(m.length))
+      val seq = mutate(m.seq)
+      selector ! Select(new GeneticSequence(seq))
     }
     case msg => {
       log.error("Received unknown message: "+msg)
     }
   }
 
-//  def mutate: immutable.IndexedSeq[Float] => immutable.IndexedSeq[Float] = {
-//    case IndexedSeq() => immutable.IndexedSeq[Float]()
-//    case seq => {
-//      val n = Random.nextInt(seq.length)
-//      val k = Random.nextInt() % seq.length
-//      val left = seq.take(n)
-//      val elem = seq(n)+k
-//      val right = seq.takeRight(seq.length-(n+1))
-//      (left :+ elem) ++ right
-//    }
-//  }
+  def mutate: immutable.IndexedSeq[Float] => immutable.IndexedSeq[Float] = {
+    case immutable.IndexedSeq() => immutable.IndexedSeq[Float]()
+    case seq => {
+      val n = Random.nextInt(seq.length)
+      val k = Random.nextFloat()
+      val left = seq.take(n)
+      val elem = if (Random.nextBoolean()) {
+        val t = seq(n)+k
+        if (t > 1) 1 else t
+      } else {
+        val t = seq(n)-k
+        if (t < 0) 0 else t
+      }
+      val right = seq.takeRight(seq.length-(n+1))
+      (left :+ elem) ++ right
+    }
+  }
 
 }
